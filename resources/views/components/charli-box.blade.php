ab90d8f2c934e15f69a018bec0e20f228fb1f45e
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-06-26 16:06:07 -0600

    accessibility

--- resources/views/components/charli-box.blade.php
+++ resources/views/components/charli-box.blade.php
@@ -1,7 +1,7 @@
 
 <div {{ $attributes->merge(['class' => " bob"]) }}>
     @for ($i = 0; $i < $iterations; $i++)
-        <center id="box-{{$1}}"> {{ $slot }} </center>
+        <center id="box-{{$i}}"> {{ $slot }} </center>
     @endfor
     <center style="width: 100%">{{ $footer }} </center>
 </div>
